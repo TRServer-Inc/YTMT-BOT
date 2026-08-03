4165a5401f808a56c7c6ee851bae3665209ad04e
TRServer Inc. <metoserin8989@gmail.com>
2026-08-03 13:27:08 +0300

    Fix language setting variable usage in dil command

--- commands/y!dil.js
+++ commands/y!dil.js
@@ -2,16 +2,17 @@ const { setUserLanguage, getText } = require('../helpers/language');
 
 module.exports = {
     name: 'dil',
-    description: 'Kendi kişisel dilini ayarlar (tr / en).',
+    description: 'Kendi kişisel dilini değiştirir (tr / en).',
     async execute(message, args, client) {
+        const userId = message.author.id;
         const secilenDil = args[0]?.toLowerCase();
 
         if (!secilenDil || (secilenDil !== 'tr' && secilenDil !== 'en')) {
-            return message.reply(getText(message.author.id, 'lang_usage'));
+            return message.reply(getText(userId, 'lang_usage'));
         }
 
-        setUserLanguage(message.author.id, secilenDil);
+        setUserLanguage(userId, secilenDil);
 
-        return message.reply(getText(message.author.id, 'lang_changed'));
+        return message.reply(getText(userId, 'lang_changed'));
     }
 };
