e4fb9caa29a1cef263cd50c5ccb26d70a63b77ed
TRServer Inc. <metoserin8989@gmail.com>
2026-08-06 22:15:34 +0300

    Delete commands directory

--- commands/sa.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    name: 'sa',
-    async execute(message, args, client) {
-        return message.reply('Aleyküm Selam, hoş geldin!');
-    }
-};
--- commands/y!ping.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    name: 'ping',
-    async execute(message, args, client) {
-        return message.reply(`Pong! 🏓 Gecikme: ${client.ws.ping}ms`);
-    }
-};
